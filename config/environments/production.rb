87b18cff7cfda57bbc34894c06945dc00f3a624a
Don Buchanan <mail@donaldbuchanan.com>
2011-09-29 13:19:32 +1000

    verbose

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Lolscript::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -42,7 +42,7 @@ Lolscript::Application.configure do
   config.cache_store = :dalli_store
 
   memcache_servers = ENV['MEMCACHE_SERVERS'] || 'localhost'
-  config.middleware.use Rack::Cache, :metastore => "memcached://#{memcache_servers}", :entitystore => "memcached://#{memcache_servers}" 
+  config.middleware.use Rack::Cache, :metastore => "memcached://#{memcache_servers}", :entitystore => "memcached://#{memcache_servers}" , :verbose => true
   
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
